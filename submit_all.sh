1579e55ad535c1e270490c0ed361e11c78b73afd
David Klein <kleinjdavid@gmail.com>
2024-02-18 19:42:51 -0800

    add ginkgo mb sizes to options

--- submit_all.sh
+++ submit_all.sh
@@ -138,7 +138,7 @@ while [ "$1" != "" ]; do
         --element )             ELEMENT=1
                                 ;;
         --ginkgo_mb_sizes ) 	shift
-                                GINKGO_MB_SIZES=( $(echo $1 | sed 's/,/ /g') )
+                                GINKGO_MB_ARRAY=( $(echo $1 | sed 's/,/ /g') )
                                 ;;
         --step )		        shift
                                 STEP=$1
@@ -296,6 +296,10 @@ fi
 if [ -z $NORMAL_SAMPLE_NAME ]; then
     echo "Normal sample name not found, necessary for germline variant calling. Please specify using --normal_sample_name. Exiting with code 1"
     exit 1
+fi
+if [ ! -z $GINKGO_MB_ARRAY -eq 1 ]; then
+    GINKGO_MB_STRING=$( IFS=$':'; echo "${GINKGO_MB_ARRAY[*]}" )
+    OPTIONS+=( "--ginkgo_mb_sizes $GINKGO_MB_STRING" ) 
 fi
 
 # On first run, stdout all options from OPTIONS variable for pipeline resubmission parameters/arguments
@@ -523,8 +527,8 @@ elif [ $STEP -eq 3 ] && [ $TEMP_ARRAY_START -eq 0 ]; then
             --bam_dir $SCRATCH_DIR --scratch_dir ${SCRATCH_DIR}/ginkgo_5M --bam_regex .5M.bam --bam_suffix .5M.bam
         echo "Asynchronous ginkgo for 5 million reads submitted"
     fi
-    if [ $TARGETED -eq 0 ] && [ ! -z $GINKGO_MB_SIZES ]; then
-        for MB_SIZE in ${GINKGO_MB_SIZES[@]}; do
+    if [ $TARGETED -eq 0 ] && [ ! -z $GINKGO_MB_ARRAY ]; then
+        for MB_SIZE in ${GINKGO_MB_ARRAY[@]}; do
             echo -e "\nsbatch -e ${STD_ERR_OUT_DIR}/%A_%x.err -o ${STD_ERR_OUT_DIR}/%A_%x.out \
                 ${SCRIPT_DIR}/3_ginkgo_cnv.sh \
                 --bam_dir $SCRATCH_DIR --scratch_dir ${SCRATCH_DIR}/ginkgo_${MB_SIZE}M --bam_regex $BAM_SUFFIX \
